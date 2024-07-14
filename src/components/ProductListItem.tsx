71b4999317014a9339345f2e2025e3b5c7a49e04
dkueng <davidwichtig@gmx.at>
2024-07-14 17:15:08 +0200

    Created types

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,12 +1,19 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Colors from '@/src/constants/Colors';
+import { Product } from '../types';
 
-const ProductListItem = ({product}) => {
+export const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+
+type ProductListItemProps = {
+    product: Product
+}
+
+const ProductListItem = (props: ProductListItemProps) => {
   return(
     <View style={styles.container}>
-      <Image source={{uri: product.image}} style={styles.image} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
+      <Image source={{uri: props.product.image || defaultPizzaImage}} style={styles.image} />
+      <Text style={styles.title}>{props.product.name}</Text>
+      <Text style={styles.price}>${props.product.price}</Text>
     </View>
   )
 }
