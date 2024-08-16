9088856e2c04534bd1fe7f79f1f7cf16058f34e0
dkueng <davidwichtig@gmx.at>
2024-08-16 15:18:26 +0200

    Implemented QueryProvider and get for products table from supabase to load real data for admins and users

--- src/app/(admin)/menu/index.tsx
+++ src/app/(admin)/menu/index.tsx
@@ -1,12 +1,19 @@
-import { FlatList } from 'react-native';
-import products from '@/assets/data/products';
+import { ActivityIndicator, FlatList, Text } from 'react-native';
 import ProductListItem from '@/src/components/ProductListItem';
-
-const product = products[0];
+import { useProductList } from '@/src/api/products';
 
 export default function MenuScreen() {
+  const {data: products, error, isLoading} = useProductList();
+
+  if(isLoading) {
+    return <ActivityIndicator />
+  }
+
+  if(error) {
+    return <Text>Failed to fetch products!</Text>
+  }
   return (
-    <FlatList 
+    <FlatList
       data={products}
       renderItem={({item}) => <ProductListItem product={item} />}
       numColumns={2}
